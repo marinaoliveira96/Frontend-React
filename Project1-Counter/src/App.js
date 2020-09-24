218b7857c348fd1cbdf732de31fd9feb1c4064e9
Marina Oliveira <marina09oliveira@gmail.com>
2020-09-24 19:08:42 -0300

    Toggle Theme - Context

--- Project1-Counter/src/App.js
+++ Project1-Counter/src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Counter from './Counter';
 import CounterHooks from './CounterHooks';
 
+export const ThemeContext = React.createContext();
 function App() {
+  const [theme, setTheme] = useState('red');
   return (
-    <>
+    <ThemeContext.Provider value={{ backgroundColor: theme }}>
       Counter
       <Counter initialCount={0} />
       CounterHooks
       <CounterHooks initialCount={5} />
-    </>
+      <button
+        onClick={() =>
+          setTheme((prev) => {
+            return prev === 'red' ? 'blue' : 'red';
+          })
+        }
+      >
+        Toggle Theme
+      </button>
+    </ThemeContext.Provider>
   );
 }
 
